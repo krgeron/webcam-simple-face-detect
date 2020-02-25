71a2b37845171438e20b8cb001d84eca0aa1886e
Geron, KennyRic <KennyRic.Geron@asurion.com>
2020-02-25 11:00:35 +0800

    Initial commit

--- webcam.py
+++ webcam.py
@@ -1,8 +1,11 @@
 import cv2
 import sys
 
+eyePath = "haarcascade_eye.xml"
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
+eye_cascade = cv2.cv2.CascadeClassifier(eyePath)
+
 
 video_capture = cv2.VideoCapture(0)
 
@@ -12,17 +15,25 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
-
+    #faces = faceCascade.detectMultiScale(
+    #    gray,
+    #    scaleFactor=1.1,
+    #    minNeighbors=5,
+    #    minSize=(30, 30),
+    #    flags=cv2.CASCADE_SCALE_IMAGE
+    #)
+    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+    print(type(faces))
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        img = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        print('\a')
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
